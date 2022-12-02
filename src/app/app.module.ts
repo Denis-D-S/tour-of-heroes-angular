a35621f3ed953d8ca59d3610ca2e3c4bf01e87fb
Denis-D-S <denisduartedasilva@hotmail.com>
2022-12-02 13:44:01 -0300

    material atualizado para o 15 corretamente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,12 @@ import {FlexModule} from "@angular/flex-layout";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import { MessagesComponent } from './messages/messages.component';
-import {MatButtonModule} from "@angular/material/button";
-import {MatListModule} from "@angular/material/list";
+import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
+import {MatLegacyListModule as MatListModule} from "@angular/material/legacy-list";
 import {MatExpansionModule} from "@angular/material/expansion";
-import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatLegacyTooltipModule as MatTooltipModule} from "@angular/material/legacy-tooltip";
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {MatCardModule} from "@angular/material/card";
+import {MatLegacyCardModule as MatCardModule} from "@angular/material/legacy-card";
 
 @NgModule({
   declarations: [
